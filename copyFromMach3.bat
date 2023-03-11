6d91a57f41545e9d01bb3cf74af4b36d99441df2
Thomas <tpo@tpo.cc>
2023-03-11 18:03:04 +0100

    Add copyTo/FromMach3 batchfiles

---
+++ copyFromMach3.bat
@@ -0,0 +1,5 @@
+copy C:\Mach3\PA_Screen.set .
+xcopy "C:\Mach3\Bitmaps\PA UI\*" ".\Bitmaps\PA UI" /Y
+copy C:\Mach3\Bitmaps\splash.bmp ".\Bitmaps" 
+
+pause
---
+++ copyToMach3.bat
@@ -0,0 +1,5 @@
+copy PA_Screen.set C:\Mach3
+xcopy "Bitmaps\PA UI\*" "C:\Mach3\Bitmaps\PA UI" /Y
+copy "Bitmaps\splash.bmp" C:\Mach3\Bitmaps
+
+pause
